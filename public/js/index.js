ee6806cb6cd407a4074c73e83db274906beee307
juune622 <juune622@naver.com>
2021-09-11 14:33:22 +0900

    ...

---
+++ public/js/index.js
@@ -0,0 +1,8 @@
+//웹사이트 주소 체제:
+
+// openweathermap : f1856687e44a2152718f62479b072e09
+//kakao : e92ce151565a21f7fded660942cbf7c8
+
+//7days: // 7days: https://api.openweathermap.org/data/2.5/onecall?lat=38&lon=127&appid=f1856687e44a2152718f62479b072e09&units=metric&exclude=minutely,hourly
+
+//http://openweathermap.org/img/wn/10d.png
